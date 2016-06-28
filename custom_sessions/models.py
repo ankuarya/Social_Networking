88a9261f5e9aa11e786ee1d1f8c36ba589674451
neeraj <professional210@gmail.com>
2016-06-28 15:49:25 +0530

    session model created

--- custom_sessions/models.py
+++ custom_sessions/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class CustomSessions(models.Model):
+    username = models.CharField(max_length=255)
+    login_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.username
